3194064856d7da7ce0ebebc694801fa382f21d41
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2023-04-09 00:33:37 +0300

    5.20. Больше подробностей (часть 2)

--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -1,1 +1,7 @@
-export const getRatingColor = (rating: number) => (Math.round(rating) * 100) / 5;
+import dayjs from 'dayjs';
+
+export const getRatingColor = (rating: number) =>
+  (Math.round(rating) * 100) / 5;
+
+export const humanizeDate = (date: string, format: string) =>
+  dayjs(date).format(format);
