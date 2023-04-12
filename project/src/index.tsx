f8b53596cbe48796d144477b5d9c0864fb4af9e3
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2023-04-12 19:42:48 +0300

    6.15. Контроль и ограничения (часть 1)

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
-import { offers } from './mocks/offers';
-
-const Setting = {
-  offersCount: 6,
-} as const;
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +10,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offersCount={Setting.offersCount} offers={offers} />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
