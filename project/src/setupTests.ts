0a24a0e19e1e08a9151d2def84cb771de8060e75
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2023-05-03 17:24:26 +0300

    для пр

--- project/src/setupTests.ts
+++ project/src/setupTests.ts
@@ -2,6 +2,4 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-// HTML-Academy пж уберите третий пункт из инструкции по отпраке проекта на проверку наставнику
-// при нажатии 'Обновить архив' после '  Найти и принять пуллркувест' оно тупо не дает отправить проект на проверку 
 import '@testing-library/jest-dom';
